9ff18f1c3274cb622c759674fdd821e368e77f70
Stephenca290 <135217719+Stephenca290@users.noreply.github.com>
2025-04-08 22:55:05 +0530

    Update and rename pdf_utils.py to pdf_parser.py

--- utils/pdf_parser.py
+++ utils/pdf_parser.py
@@ -1,7 +1,7 @@
-import PyPDF2
+from PyPDF2 import PdfReader
 
-def extract_text_from_pdf(file):
-    reader = PyPDF2.PdfReader(file)
+def extract_text_from_pdf(pdf_file):
+    reader = PdfReader(pdf_file)
     text = ""
     for page in reader.pages:
         text += page.extract_text() or ""
